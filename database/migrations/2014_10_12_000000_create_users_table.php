f7ee5421e2d0b43161f77752c8ae757073e60437
Rizka Nuzulia <rizka_lia@mhs.unsyiah.ac.id>
2022-04-25 06:50:04 +0700

    Database user sikat

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('no_hp');
+            $table->string('username');
             $table->string('password');
+            $table->string('konfirmasi_password');
             $table->rememberToken();
             $table->timestamps();
         });
